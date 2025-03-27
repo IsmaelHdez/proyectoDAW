b23a2487e0a6d6597782b84c7cd35116adc1aa3d
Endovelico <capitanendovelico@yahoo.es>
2025-03-27 23:28:55 +0100

    ñ añadida

--- JS/validacion_admin.js
+++ JS/validacion_admin.js
@@ -1,6 +1,6 @@
 // Función para validar nombres o apellidos
        function validar_Nombre(nombre) {
-        const regex = /^[a-zA-Z\s]+$/;
+        const regex = /^[a-zA-Z\sñÑ]+$/;
         return regex.test(nombre);
     }
 
