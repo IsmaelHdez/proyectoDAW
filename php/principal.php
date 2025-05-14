a7949226f549b3dacb34de5707f1ddce7b93ad10
IsmaelHdez <hernandezhernandezismael@gmail.com>
2025-05-14 20:02:09 +0100

    header_footer2

--- php/principal.php
+++ php/principal.php
@@ -11,14 +11,8 @@
         <img src="https://res.cloudinary.com/dup8qzlzv/image/upload/v1742377568/portada_ap7bfr.png" alt="">
     </div>
     <?php
-    // Incluye el header adecuado según si la variable $_SESSION['usuario'] tiene contenido
-    if (isset($_SESSION['usuario']) && !empty($_SESSION['usuario'])) {
-        require("header_alta.php");
-    } else {
-        require("../html/header.html");
-    }
+    
         require("noticias_total.php");
-        require("../html/footer.html");
     ?>
 </body>
 </html>
